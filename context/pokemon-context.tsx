b782887263ffb1b37b14ccca510333560e007008
Eduardo Gaspari <eduardogaspari.eu@gmail.com>
2025-06-30 14:02:10 -0300

    chore: removes test files, clean pokemon context

--- context/pokemon-context.tsx
+++ context/pokemon-context.tsx
@@ -1,25 +1,12 @@
 'use client'
 
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  useMemo,
-} from 'react'
-import { ApolloError, useQuery, useLazyQuery } from '@apollo/client'
-import { GET_POKEMON_SPRITE } from '@/graphql/queries/get-pokemon-sprite'
+import React, { createContext, useContext, useState, useMemo } from 'react'
+import { ApolloError, useQuery } from '@apollo/client'
 import { GET_POKEMON_DETAILS } from '@/graphql/queries/get-pokemon-details'
-import { GET_EVOLUTION_CHAIN } from '@/graphql/queries/get-evolution-chain'
 
 interface Pokemon {
   id: number
   name: string
-}
-
-interface EvolutionSprite {
-  name: string
-  sprite: string
 }
 
 interface PokemonDetails {
@@ -31,23 +18,6 @@ interface PokemonDetails {
   types: { type: { name: string } }[]
   stats: { base_stat: number; stat: { name: string } }[]
   sprites: { front_default: string }
-  evolutionChain: EvolutionSprite[]
-}
-
-export interface Species {
-  name: string
-  url: string
-}
-
-export interface EvolutionNode {
-  evolves_to: EvolutionNode[]
-  is_baby: boolean
-  species: Species
-}
-
-export interface EvolutionChain {
-  chain: EvolutionNode
-  id: number
 }
 
 interface PokemonContextType {
@@ -59,55 +29,6 @@ interface PokemonContextType {
 }
 
 const PokemonContext = createContext<PokemonContextType | undefined>(undefined)
-
-const extractEvolutionNames = (chain: EvolutionNode): string[] => {
-  const names: string[] = []
-  const traverse = (node: EvolutionNode) => {
-    if (node?.species?.name) names.push(node.species.name)
-    node?.evolves_to?.forEach(traverse)
-  }
-
-  traverse(chain)
-  return names
-}
-
-function useEvolutionSprites(names: string[]) {
-  const [sprites, setSprites] = useState<EvolutionSprite[]>([])
-  const [loading, setLoading] = useState(false)
-  const [fetchSprite] = useLazyQuery(GET_POKEMON_SPRITE)
-
-  useEffect(() => {
-    if (!names.length) return setSprites([])
-
-    const loadSprites = async () => {
-      setLoading(true)
-      const results: EvolutionSprite[] = []
-
-      for (const name of names) {
-        try {
-          const { data } = await fetchSprite({ variables: { name } })
-          const sprite =
-            data?.pokemon?.sprites?.front_default ||
-            `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name}.png`
-
-          results.push({ name, sprite })
-        } catch {
-          results.push({
-            name,
-            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${name}.png`,
-          })
-        }
-      }
-
-      setSprites(results)
-      setLoading(false)
-    }
-
-    loadSprites()
-  }, [names, fetchSprite])
-
-  return { sprites, loading }
-}
 
 export function PokemonProvider({ children }: { children: React.ReactNode }) {
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null)
@@ -121,32 +42,9 @@ export function PokemonProvider({ children }: { children: React.ReactNode }) {
     skip: !selectedPokemon,
   })
 
-  const { data: evolutionData, loading: loadingEvolution } = useQuery(
-    GET_EVOLUTION_CHAIN,
-    {
-      variables: { id: selectedPokemon?.id?.toString() },
-      skip: !selectedPokemon,
-    },
-  )
-
-  console.log(evolutionData)
-
-  const evolutionNames = useMemo(() => {
-    return (
-      extractEvolutionNames(evolutionData?.evolutionChain?.response?.chain) ||
-      []
-    )
-  }, [evolutionData])
-
-  const { sprites: evolutionSprites, loading: loadingSprites } =
-    useEvolutionSprites(evolutionNames)
-
   const pokemonDetails: PokemonDetails | null = useMemo(() => {
-    if (!detailsData?.pokemon) return null
-    return { ...detailsData.pokemon, evolutionChain: evolutionSprites }
-  }, [detailsData, evolutionSprites])
-
-  const isLoading = loadingDetails || loadingEvolution || loadingSprites
+    return detailsData?.pokemon ?? null
+  }, [detailsData])
 
   return (
     <PokemonContext.Provider
@@ -154,7 +52,7 @@ export function PokemonProvider({ children }: { children: React.ReactNode }) {
         selectedPokemon,
         setSelectedPokemon,
         pokemonDetails,
-        loadingDetails: isLoading,
+        loadingDetails,
         errorDetails,
       }}
     >
--- graphql/queries/get-evolution-chain.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client'
-
-export const GET_EVOLUTION_CHAIN = gql`
-  query GetEvolutionChain($id: String!) {
-    evolutionChain(id: $id) {
-      response
-    }
-  }
-`
